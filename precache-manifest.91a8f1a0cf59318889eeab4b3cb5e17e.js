71a9bed0d2495b7f17daac48df0004efb4010760
sarpisik <contact@sarpisik.com>
2019-09-09 21:35:16 +0300

    Updates

---
+++ precache-manifest.91a8f1a0cf59318889eeab4b3cb5e17e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13b45f472f5e2ff32a8de092fb720f9f",
+    "url": "/react-hooks-tetris/index.html"
+  },
+  {
+    "revision": "af360a25f02b9e008ac0",
+    "url": "/react-hooks-tetris/static/css/main.ce50fb2c.chunk.css"
+  },
+  {
+    "revision": "78b11a690ccf2e718e77",
+    "url": "/react-hooks-tetris/static/js/2.585972d8.chunk.js"
+  },
+  {
+    "revision": "af360a25f02b9e008ac0",
+    "url": "/react-hooks-tetris/static/js/main.58406acb.chunk.js"
+  },
+  {
+    "revision": "2ecb79379b9ca393e6aa",
+    "url": "/react-hooks-tetris/static/js/runtime~main.43bb5602.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/react-hooks-tetris/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/react-hooks-tetris/static/media/bg.2810fe6b.png"
+  }
+]);
